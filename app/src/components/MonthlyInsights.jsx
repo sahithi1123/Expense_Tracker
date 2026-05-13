1d31b06762a9a3c30282e80242ab81fd4dc4f8c3
sahithi1123 <hanumantusahithi30@gmail.com>
2026-05-13 19:56:06 +0530

    Update MonthlyInsights.jsx

--- app/src/components/MonthlyInsights.jsx
+++ app/src/components/MonthlyInsights.jsx
@@ -75,13 +75,22 @@ function MonthlyInsights() {
 
                     </select>
 
-                    <input
-                        type="number"
-                        placeholder="Enter Year"
-                        value={year}
-                        onChange={(e) => setYear(e.target.value)}
-                        style={styles.input}
-                    />
+                    <select
+                    value={year}
+                    onChange={(e) => setYear(e.target.value)}
+                    style={styles.input}
+                    >
+
+                    <option value="">
+                    Select Year
+                    </option>
+
+                    <option value="2023">2023</option>
+                    <option value="2024">2024</option>
+                    <option value="2025">2025</option>
+                    <option value="2026">2026</option>
+
+                    </select>
 
                     <button
                         style={styles.button}
